a795d714e5810e88b16c2c2493fbd542af186952
Ross Malpass <ross@ombori.com>
2022-07-16 12:10:23 +0100

    setting up the site

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,48 +1,71 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import styled from "@emotion/styled"
+import { Global } from "@emotion/react"
+import { ThemeProvider } from "theme-ui"
+import theme from "../gatsby-plugin-theme-ui"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
-    <>
-      {/* <Header siteTitle={data.site.siteMetadata?.title || `Title`} /> */}
-      <main>{children}</main>
-      {/* <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer> */}
-    </>
+    <ThemeProvider theme={theme}>
+      <Global
+        styles={theme => ({
+          "html, body, p, ol, ul, li, dl, dt, dd, blockquote, figure, fieldset, legend, textarea, pre, iframe, hr, h1, h2, h3, h4, h5, h6":
+            {
+              margin: 0,
+              padding: 0,
+            },
+          "h1, h2, h3, h4, h5, h6": {
+            fontSize: "100%",
+            fontWeight: "normal",
+          },
+          ul: {
+            listStyle: "none",
+          },
+          "button, input, select": {
+            margin: 0,
+          },
+          html: {
+            boxSizing: "border-box",
+          },
+          "*, *::before, *::after": {
+            boxSizing: "inherit",
+          },
+          "img, video": {
+            height: "auto",
+            maxWidth: "100%",
+          },
+          iframe: {
+            border: 0,
+          },
+          table: {
+            borderCollapse: "collapse",
+            borderSpacing: 0,
+          },
+          "td, th": {
+            padding: 0,
+          },
+          "html, body, #___gatsby, #gatsby-focus-wrapper, div[data-themeui-nested-provider]":
+            {
+              height: "100%",
+              background: theme.colors.primary,
+            },
+          body: {},
+        })}
+      />
+      <Wrapper>{children}</Wrapper>
+    </ThemeProvider>
   )
 }
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
 export default Layout
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 0;
+  min-height: 100%;
+  max-width: 1440px;
+  margin: 0 auto;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,13 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
-
-import Logo from "../images/logo.svg"
+import Header from "../components/header"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <img src={Logo} alt="$#*!" />
-    </div>
+    <Header />
   </Layout>
 )
 
